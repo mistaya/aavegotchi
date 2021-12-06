9204b4337349556c82a4d6fb5a9e7dc88f43df3b
Mistaya <10831655+mistaya@users.noreply.github.com>
2021-12-06 21:14:15 +0000

    Add mobile support to map zooming

--- realm/src/components/CitaadelMap.vue
+++ realm/src/components/CitaadelMap.vue
@@ -536,40 +536,50 @@
             List View
           </button>
         </div>
-        <svg
-          v-show="viewMode === 'map'"
-          ref="svgRef"
-          xmlns="http://www.w3.org/2000/svg"
-          :viewBox="INITIAL_DISPLAY_VIEWBOX"
-          class="map-svg"
-          :class="{
-            'map-svg--filter-hide': filters.displayMode === 'hide',
-            'map-svg--filter-outline': filters.displayMode === 'outline'
-          }"
-          :style="{
-            'aspect-ratio': INITIAL_DISPLAY_ASPECT_RATIO
-          }"
-        >
-          <a
-            v-for="parcel in parcelsToDisplay"
-            :key="parcel.id"
-            xlink:href="#"
-            @click.prevent="onClickParcel(parcel)"
+        <div v-show="viewMode === 'map'">
+          <svg
+            ref="svgRef"
+            xmlns="http://www.w3.org/2000/svg"
+            :viewBox="INITIAL_DISPLAY_VIEWBOX"
+            class="map-svg"
+            :class="{
+              'map-svg--filter-hide': filters.displayMode === 'hide',
+              'map-svg--filter-outline': filters.displayMode === 'outline'
+            }"
+            :style="{
+              'aspect-ratio': INITIAL_DISPLAY_ASPECT_RATIO
+            }"
           >
-            <rect
-              class="parcel"
-              :class="{
-                'parcel--hidden-by-filter': !parcel.show
-              }"
-              :x="parcel.coordinateX"
-              :y="parcel.coordinateY"
-              :width="parcel.sizeLabel === 'humble' ? 8 : parcel.sizeLabel === 'reasonable' ? 16 : parcel.vertical ? 32 : 64"
-              :height="parcel.sizeLabel === 'humble' ? 8 : parcel.sizeLabel === 'reasonable' ? 16 : parcel.vertical ? 64 : 32"
-              stroke="#777"
-              :fill="parcel.color"
-            />
-          </a>
-        </svg>
+            <g>
+              <a
+                v-for="parcel in parcelsToDisplay"
+                :key="parcel.id"
+                xlink:href="#"
+                @click.prevent="onClickParcel(parcel)"
+              >
+                <rect
+                  class="parcel"
+                  :class="{
+                    'parcel--hidden-by-filter': !parcel.show
+                  }"
+                  :x="parcel.coordinateX"
+                  :y="parcel.coordinateY"
+                  :width="parcel.sizeLabel === 'humble' ? 8 : parcel.sizeLabel === 'reasonable' ? 16 : parcel.vertical ? 32 : 64"
+                  :height="parcel.sizeLabel === 'humble' ? 8 : parcel.sizeLabel === 'reasonable' ? 16 : parcel.vertical ? 64 : 32"
+                  stroke="#777"
+                  :fill="parcel.color"
+                />
+              </a>
+            </g>
+          </svg>
+          <button
+            type="button"
+            style="margin-top: 8px"
+            @click="resetZoom"
+          >
+            Reset map zoom
+          </button>
+        </div>
         <div v-if="viewMode === 'list'">
           <template v-if="!listParcelsToDisplay.length">
             No parcels found.
@@ -611,6 +621,7 @@ import { ref, computed, onMounted } from 'vue'
 import useParcels from '@/data/useParcels'
 import useAuctions from '@/data/useAuctions'
 import svgPanZoom from 'svg-pan-zoom'
+import Hammer from 'hammerjs'
 import { scaleSequential } from 'd3-scale'
 import { interpolateViridis, interpolateBlues, interpolateInferno, interpolateCividis, interpolateSpectral, interpolateTurbo, interpolateRainbow } from 'd3-scale-chromatic'
 import { format } from 'date-fns'
@@ -965,7 +976,6 @@ export default {
 
     const onClickParcel = function (parcel) {
       selectedParcelId.value = parcel.id
-      parcelDetailsRef.value.scrollIntoView(false)
     }
     const selectedParcel = computed(() =>
       getParcelDetails(
@@ -1012,15 +1022,93 @@ export default {
       return totalPerBidder
     })
 
+    const panZoom = ref(null)
+    const resetZoom = function () {
+      if (!panZoom.value) { return }
+      panZoom.value.reset()
+    }
     onMounted(() => {
-      svgPanZoom(svgRef.value, {
-        dblClickZoomEnabled: false
+      // mobile example code https://bumbu.me/svg-pan-zoom/demo/mobile.html
+      // limit panning example code https://bumbu.me/svg-pan-zoom/demo/limit-pan.html
+      panZoom.value = svgPanZoom(svgRef.value, {
+        dblClickZoomEnabled: false,
+        beforePan: function (oldPan, newPan) {
+          const gutterWidth = 100
+          const gutterHeight = 100
+          // Computed variables
+          const sizes = this.getSizes()
+          const leftLimit = -((sizes.viewBox.x + sizes.viewBox.width) * sizes.realZoom) + gutterWidth
+          const rightLimit = sizes.width - gutterWidth - (sizes.viewBox.x * sizes.realZoom)
+          const topLimit = -((sizes.viewBox.y + sizes.viewBox.height) * sizes.realZoom) + gutterHeight
+          const bottomLimit = sizes.height - gutterHeight - (sizes.viewBox.y * sizes.realZoom)
+
+          const customPan = {
+            x: Math.max(leftLimit, Math.min(rightLimit, newPan.x)),
+            y: Math.max(topLimit, Math.min(bottomLimit, newPan.y))
+          }
+          return customPan
+        },
+        customEventsHandler: {
+          haltEventListeners: ['touchstart', 'touchend', 'touchmove', 'touchleave', 'touchcancel'],
+          init: function (options) {
+            const instance = options.instance
+            let initialScale = 1
+            let pannedX = 0
+            let pannedY = 0
+
+            // Init Hammer
+            // Listen only for pointer and touch events
+            this.hammer = Hammer(options.svgElement, {
+              inputClass: Hammer.SUPPORT_POINTER_EVENTS ? Hammer.PointerEventInput : Hammer.TouchInput
+            })
+
+            // Enable pinch
+            this.hammer.get('pinch').set({ enable: true })
+
+            // Handle double tap
+            this.hammer.on('doubletap', function (ev) {
+              instance.zoomIn()
+            })
+
+            // Handle pan
+            this.hammer.on('panstart panmove', function (ev) {
+              // On pan start reset panned variables
+              if (ev.type === 'panstart') {
+                pannedX = 0
+                pannedY = 0
+              }
+
+              // Pan only the difference
+              instance.panBy({ x: ev.deltaX - pannedX, y: ev.deltaY - pannedY })
+              pannedX = ev.deltaX
+              pannedY = ev.deltaY
+            })
+
+            // Handle pinch
+            this.hammer.on('pinchstart pinchmove', function (ev) {
+              // On pinch start remember initial zoom
+              if (ev.type === 'pinchstart') {
+                initialScale = instance.getZoom()
+                instance.zoomAtPoint(initialScale * ev.scale, { x: ev.center.x, y: ev.center.y })
+              }
+
+              instance.zoomAtPoint(initialScale * ev.scale, { x: ev.center.x, y: ev.center.y })
+            })
+
+            // Prevent moving the page on some devices when panning over SVG
+            options.svgElement.addEventListener('touchmove', function (e) { e.preventDefault() })
+          },
+          destroy: function () {
+            this.hammer.destroy()
+          }
+        }
       })
     })
     return {
       INITIAL_DISPLAY_VIEWBOX,
       INITIAL_DISPLAY_ASPECT_RATIO,
       svgRef,
+      resetZoom,
       parcelDetailsRef,
       viewMode,
       parcelsToDisplay: filteredParcelsToDisplay,
