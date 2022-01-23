8059f4ef8897f6d952de8236c9b1f8bcb5b0f24d
Mistaya <10831655+mistaya@users.noreply.github.com>
2022-01-23 17:23:26 +0000

    Add alchemica deposits to map

--- realm/src/components/MapConfig.vue
+++ realm/src/components/MapConfig.vue
@@ -23,30 +23,50 @@
     </div>
 
     <div style="margin-top: 15px">
-      Map features:
+      Map features - show...
       <div
         v-for="feature in [
           { id: 'Roads', label: 'roads' },
           { id: 'Walls', label: 'walls' },
           { id: 'Paartners', label: 'paartner parcels' },
-          { id: 'Landmarks', label: 'landmarks' }
+          { id: 'Landmarks', label: 'landmarks' },
+          { id: 'AlchemicaFud', label: 'FUD deposits' },
+          { id: 'AlchemicaFomo', label: 'FOMO deposits' },
+          { id: 'AlchemicaAlpha', label: 'ALPHA deposits' },
+          { id: 'AlchemicaKek', label: 'KEK deposits' },
         ]"
         :key="feature.id"
         style="margin-top: 5px"
       >
-        <label style="margin-right: 7px">
+        <label style="margin-right: 7px; text-transform: capitalize;">
           <input
             type="checkbox"
             :checked="modelValue[`show${feature.id}`]"
             @input="showChanged($event.target.checked, `show${feature.id}`)"
           />
-          Show {{ feature.label }}
+          {{ feature.label }}
         </label>
         <input
           type="color"
           :aria-label="`Color for ${feature.label}`"
           :value="modelValue[`color${feature.id}`]"
           @input="debouncedColorChanged($event.target.value, `color${feature.id}`)"
+        >
+      </div>
+      <div style="margin-top: 10px">
+        <label style="margin-right: 7px">
+          <input
+            type="checkbox"
+            :checked="modelValue.useOneAlchemicaColor"
+            @input="showChanged($event.target.checked, 'useOneAlchemicaColor')"
+          />
+          Same color for all deposits:
+        </label>
+        <input
+          type="color"
+          :aria-label="`Color for all alchemica deposits`"
+          :value="modelValue.colorAlchemica"
+          @input="debouncedColorChanged($event.target.value, 'colorAlchemica')"
         >
       </div>
     </div>
@@ -63,10 +83,20 @@ const getDefaultValue = function () {
     showWalls: true,
     showPaartners: true,
     showLandmarks: true,
+    showAlchemicaFud: true,
+    showAlchemicaFomo: true,
+    showAlchemicaAlpha: true,
+    showAlchemicaKek: true,
     colorRoads: '#bbbbbb',
     colorWalls: '#000000',
     colorPaartners: '#bf91ff',
     colorLandmarks: '#FA34F3',
+    colorAlchemicaFud: '#C8E4C8',
+    colorAlchemicaFomo: '#F0DAD1',
+    colorAlchemicaAlpha: '#D7EEEE',
+    colorAlchemicaKek: '#F0D9F2',
+    useOneAlchemicaColor: false,
+    colorAlchemica: '#E3E3E3',
     flagSelected: false
   }
 }
