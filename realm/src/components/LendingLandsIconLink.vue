2205b486691c1bbada9be6bc876b32c7bf8c148c
Mistaya <10831655+mistaya@users.noreply.github.com>
2022-05-24 23:12:37 +0100

    Add hover for land icon

--- realm/src/components/LendingLandsIconLink.vue
+++ realm/src/components/LendingLandsIconLink.vue
@@ -3,6 +3,7 @@
     v-if="address"
     :to="{ name: 'lending-lands', query: { address } }"
     :title="`View lands belonging to ${address.substring(0, 5)}`"
+    class="lending-lands-icon-link"
   >
     <SiteIcon
       name="home"
@@ -21,3 +22,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .lending-lands-icon-link:hover {
+    filter: drop-shadow(1px 1px 1px rgba(255, 150, 255, 1));
+  }
+</style>
