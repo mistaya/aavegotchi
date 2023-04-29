5b3ca4ce5ee98178f15799697d88049e6d8a2d38
Mistaya <10831655+mistaya@users.noreply.github.com>
2023-04-29 12:54:14 +0100

    Update lendings query to use timeAgreed 0 instead of null

--- realm/src/pages/lending/LendingAvailablePage.vue
+++ realm/src/pages/lending/LendingAvailablePage.vue
@@ -720,7 +720,7 @@ export default {
 
       const query = `
       {gotchiLendings(first: ${fetchPageSize.value} ${orderQuery}, where: {
-        timeAgreed: null,
+        timeAgreed: 0,
         cancelled: false,
         ${upfrontQuery}
         ${borrowerSplitQuery}
