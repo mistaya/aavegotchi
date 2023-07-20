62f938bc565e5e3f29ba302559afdee15a28d2df
Mistaya <10831655+mistaya@users.noreply.github.com>
2023-07-20 21:51:53 +0100

    Use core-matic subgraph v1.4.0
    This seems to have good data, but 1.4.5-dev leads to bugs

--- realm/src/data/apis.js
+++ realm/src/data/apis.js
@@ -1,6 +1,8 @@
 export default {
   // CORE_MATIC_SUBGRAPH: 'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-core-matic',
-  CORE_MATIC_SUBGRAPH: 'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/api',
+  // CORE_MATIC_SUBGRAPH: 'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/api',
+  // CORE_MATIC_SUBGRAPH: 'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/version/v1.4.5-dev/api',
+  CORE_MATIC_SUBGRAPH: 'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/version/v1.4.0/api',
   LENDING_SUBGRAPH: 'https://api.thegraph.com/subgraphs/name/sudeepb02/gotchi-lending',
   GOTCHIVERSE_SUBGRAPH: 'https://api.thegraph.com/subgraphs/name/aavegotchi/gotchiverse-matic',
   ETHEREUM_GOTCHIS_SUBGRAPH: 'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-ethereum',
