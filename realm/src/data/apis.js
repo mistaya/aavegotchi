fa7890c46549431447a043ace6f2df0872b2349d
Mistaya <10831655+mistaya@users.noreply.github.com>
2023-10-09 10:51:57 +0100

    Update subgraph URLs

--- realm/src/data/apis.js
+++ realm/src/data/apis.js
@@ -1,7 +1,7 @@
 export default {
   // CORE_MATIC_SUBGRAPH: 'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-core-matic',
-  CORE_MATIC_V1_SUBGRAPH: 'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/api',
-  CORE_MATIC_SUBGRAPH: 'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/version/v2.0.0/api',
+  CORE_MATIC_V1_SUBGRAPH: 'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/version/v1.0.1/api',
+  CORE_MATIC_SUBGRAPH: 'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/api',
   LENDING_SUBGRAPH: 'https://api.thegraph.com/subgraphs/name/sudeepb02/gotchi-lending',
   GOTCHIVERSE_SUBGRAPH: 'https://api.thegraph.com/subgraphs/name/aavegotchi/gotchiverse-matic',
   ETHEREUM_GOTCHIS_SUBGRAPH: 'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-ethereum',
